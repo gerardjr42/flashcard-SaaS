650dc3731d3e4ade476e27d3121b25b421b759d5
gerardjr42 <ggarciajr12@gmail.com>
2024-08-16 19:19:50 -0400

    create login functionality

--- app/page.js
+++ app/page.js
@@ -1,7 +1,29 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Container>
+      <Head>
+        <title>Flashcard SaaS</title>
+        <meta name="description" content="Create Flashcards from text" />
+      </Head>
+
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit">Loginin</Button>
+            <Button color="inherit">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 }
